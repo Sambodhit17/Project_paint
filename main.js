07a7e46164f744acbad0c123b0b3f074238438eb
Sambodhit17 <92640348+Sambodhit17@users.noreply.github.com>
2021-12-16 19:38:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+canvas =
+ document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+
+
+Color = "Red";
+Width = 2;
+
+
+canvas.addEventListener("mousedown" , my_mousedown);
+
+function my_mousedown() {
+    document.getElementById("Color").value;
+    document.getElementById("Width").value;
+    document.getElementById("input3").value;
+    MouseEvent = mousedown();
+}
+
+
+canvas.addEventListener("mousemove" , my_mousemove);
+
+function my_mousemove() {
+    
+}
